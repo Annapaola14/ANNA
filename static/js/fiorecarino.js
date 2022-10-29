fc6e74f1127a91aa4e13cfc7ded22be409e9c8d3
Annapaola14 <annapaola.amoruso@gmail.com>
2022-10-29 09:07:58 +0200

    first commit

---
+++ static/js/fiorecarino.js
@@ -0,0 +1,40 @@
+function setup(){
+	createCanvas(600,300)
+	background("green")
+}
+
+function draw(){
+	fill("orange");
+	circle(50,100,100);
+	fill("orange");
+	circle(50,200,100);
+	fill("red");
+	circle(0,150,100);
+
+
+	fill("orange");
+	circle(250,100,100);
+	fill("purple");
+	circle(350,100,100);
+	fill("orange");
+	circle(350,200,100);
+	fill("pink");
+	circle(250,200,100);
+	fill("red");
+	circle(300,150,100);
+	
+
+
+	fill("orange");
+	circle(550,100,100);
+	fill("orange");
+	circle(550,200,100);
+	fill("red");
+	circle(600,150,100);
+
+
+
+
+
+	
+}
